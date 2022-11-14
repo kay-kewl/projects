40e99ca8ac7d7142fa98dc5a54fbd2c7ee112403
kay <kkd73740305@ya.ru>
2022-11-14 13:12:28 +0300

    tehtd2

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+    # bpfgdfgpfdg
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-return float
